994e24810f191ee0d03f75c1badf759aa7536a6b
EgogorGames <EgogorGames@gmail.com>
2023-01-07 14:29:07 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -54,6 +54,6 @@ html_theme_options = {
     'includehidden': True,
     'titles_only': False,
     'github_url': 'https://github.com/EgogorGames/EasyCord/',
-    'html-logo': 'https://i.imgur.com/NGavnw7.png'
+    'html-logo': 'ajw67-0abwc.png'
 }
 
